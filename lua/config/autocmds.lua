84c2282d5619bb5b75aa7f55fed69218b394663e
Ryan Egesdahl <ryan@deriamis.net>
2025-07-20 21:16:19 -0700

    Add basic config

---
+++ lua/config/autocmds.lua
@@ -0,0 +1,7 @@
+-- Autocmds are automatically loaded on the VeryLazy event
+--
+-- Add any additional autocmds here
+-- with `vim.api.nvim_create_autocmd`
+--
+-- Or remove existing autocmds by their group name (which is prefixed with `powervim_` for the defaults)
+-- e.g. vim.api.nvim_del_augroup_by_name("powervim_wrap_spell")
