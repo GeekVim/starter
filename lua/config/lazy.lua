a9914e5f090ec9d72a5d7df0f3aa86bbd15a8e5e
Ryan Egesdahl <ryan@deriamis.net>
2025-08-16 14:44:40 -0700

    One more missing rename

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -17,12 +17,12 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
   spec = {
     -- add GeekVim and import its plugins
-    { "GeekVim/LazyVim", import = "geekvim.plugins" },
+    { "GeekVim/GeekVim", import = "geekvim.plugins" },
     -- import/override with your plugins
     { import = "plugins" },
   },
   defaults = {
-    -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
+    -- By default, only GeekVim plugins will be lazy-loaded. Your custom plugins will load during startup.
     -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
     lazy = false,
     -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
