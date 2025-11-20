a0a56dfd19672cd331d279aa20a50e695427eea3
Ryan Egesdahl <ryan@deriamis.net>
2025-11-20 11:05:15 -0800

    Update from template

--- init.lua
+++ init.lua
@@ -1,2 +1,14 @@
--- bootstrap lazy.nvim, GeekVim and your plugins
-require("config.lazy")
+vim.api.nvim_echo({
+  {
+    "Do not use this repository directly\n",
+    "ErrorMsg",
+  },
+  {
+    "Please check the docs on how to get started with GeekVim\n",
+    "WarningMsg",
+  },
+  { "Press any key to exit", "MoreMsg" },
+}, true, {})
+
+vim.fn.getchar()
+vim.cmd([[quit]])
